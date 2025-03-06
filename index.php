95152e229a6f7926512fca8d64bef94dcd05ed94
vmakre <vmakre@gmail.com>
2025-03-06 14:56:02 +0100

    style(docs): Update index.php and add Readme.md for improved documentation. (Changing stylesheets to Water.css for a darker theme and adding explanatory comments in the Readme.)

--- index.php
+++ index.php
@@ -7,10 +7,11 @@
   <meta name="viewport"
     content="width=device-width, initial-scale=1.0">
   <title>HTMX PHP Examples Index</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+  <link href="https://unpkg.com/water.css@2/out/dark.css" rel="stylesheet">
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
     rel="stylesheet"
     integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-    crossorigin="anonymous">
+    crossorigin="anonymous"> -->
   <script defer
     src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
